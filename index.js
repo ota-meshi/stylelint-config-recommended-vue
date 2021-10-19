4a58d3bc68fb73e75cc967ff0fe0793784aa6040
Yosuke Ota <otameshiyo23@gmail.com>
2021-10-19 11:57:05 +0900

    update config (#4)

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ module.exports = {
             // extends: ["stylelint-config-recommended"],
             rules: {
                 ...recommended.rules,
+                "selector-pseudo-class-no-unknown": [
+                    true,
+                    {
+                        ignorePseudoClasses: ["deep", "global"],
+                    },
+                ],
+                "selector-pseudo-element-no-unknown": [
+                    true,
+                    {
+                        ignorePseudoElements: [
+                            "v-deep",
+                            "v-global",
+                            "v-slotted",
+                        ],
+                    },
+                ],
             },
         },
     ],
--- tests/fixtures/integrations/stylelint/src/valid.vue
+++ tests/fixtures/integrations/stylelint/src/valid.vue
@@ -2,4 +2,13 @@
 a {
     color: red;
 }
+
+:global(.foo),
+::v-deep(.foo),
+::v-slotted(.foo),
+::v-global(.foo),
+:deep(.foo),
+:slotted(.foo) {
+    color: red;
+}
 </style>
