35555f636566c5665553a4553bbb427f73ee0181
ota-meshi <otameshiyo23@gmail.com>
2021-11-27 13:33:32 +0900

    Add stylelint-config-recommended-vue/scss config

---
+++ tests/fixtures/integrations/stylelint-scss/src/valid.vue
@@ -0,0 +1,31 @@
+<style lang="scss">
+a {
+    color: red;
+}
+
+// SCSS
+.b {
+    // stylelint-disable-next-line scss/at-if-no-null
+    @if $x == null {
+        color: red;
+    }
+}
+@mixin my-mixin {
+  color: blue
+}
+
+.c {
+  @include my-mixin
+}
+
+// Vue
+:global(.foo),
+::v-deep(.foo),
+::v-slotted(.foo),
+::v-global(.foo),
+:deep(.foo),
+:slotted(.foo) {
+    color: red;
+}
+
+</style>
