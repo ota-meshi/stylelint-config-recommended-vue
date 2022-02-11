d817dee99b07eea79bb0e1849eade9e217080477
yosuke ota <otameshiyo23@gmail.com>
2022-02-11 10:21:07 +0900

    Add support for Stylelint v14.5

---
+++ tests/fixtures/integrations/stylelint-v14.3/src/valid.vue
@@ -0,0 +1,14 @@
+<style>
+a {
+    color: red;
+}
+
+:global(.foo),
+::v-deep(.foo),
+::v-slotted(.foo),
+::v-global(.foo),
+:deep(.foo),
+:slotted(.foo) {
+    color: red;
+}
+</style>
--- tests/lib/for-old-stylelint.js
+++ tests/lib/for-old-stylelint.js
@@ -4,54 +4,29 @@ const { fail } = require("assert")
 const cp = require("child_process")
 const path = require("path")
 
+cp.execSync("npm pack", { stdio: "inherit" })
+const tgzName = path.resolve(
+    `stylelint-config-recommended-vue-${
+        require("../../package.json").version
+    }.tgz`,
+)
+
 const STYLELINT = `.${path.sep}node_modules${path.sep}.bin${path.sep}stylelint`
 
-describe("Integration with stylelint", () => {
+describe("Integration with stylelint v14.3", () => {
     let originalCwd
 
     before(() => {
         originalCwd = process.cwd()
         process.chdir(
-            path.join(__dirname, "../fixtures/integrations/stylelint"),
+            path.join(__dirname, "../fixtures/integrations/stylelint-v14.3"),
         )
         cp.execSync("npm i", { stdio: "inherit" })
-    })
-    after(() => {
-        process.chdir(originalCwd)
-    })
-
-    it("should lint without errors", () => {
-        cp.execSync(`${STYLELINT} src/valid.vue`, { stdio: "inherit" })
-    })
-    it("should lint with errors", () => {
-        try {
-            cp.execSync(`${STYLELINT} src/invalid.vue`, { stdio: "inherit" })
-        } catch {
-            // Expected!
-            return
-        }
-        fail("Expect an error, but without errors")
-    })
-    it("should lint with errors #2", () => {
-        try {
-            cp.execSync(`${STYLELINT} src/invalid2.vue`, { stdio: "inherit" })
-        } catch {
-            // Expected!
-            return
-        }
-        fail("Expect an error, but without errors")
-    })
-})
-
-describe("Integration with stylelint with scss", () => {
-    let originalCwd
-
-    before(() => {
-        originalCwd = process.cwd()
-        process.chdir(
-            path.join(__dirname, "../fixtures/integrations/stylelint-scss"),
+        cp.execSync(
+            "npx rimraf ./node_modules/stylelint-config-recommended-vue",
+            { stdio: "inherit" },
         )
-        cp.execSync("npm i", { stdio: "inherit" })
+        cp.execSync(`npm i ${tgzName}`, { stdio: "inherit" })
     })
     after(() => {
         process.chdir(originalCwd)
@@ -71,33 +46,31 @@ describe("Integration with stylelint with scss", () => {
     })
 })
 
-describe("Integration with stylelint-config-standard-scss", () => {
+describe("Integration with stylelint v14.4", () => {
     let originalCwd
 
     before(() => {
         originalCwd = process.cwd()
         process.chdir(
-            path.join(
-                __dirname,
-                "../fixtures/integrations/stylelint-config-standard-scss",
-            ),
+            path.join(__dirname, "../fixtures/integrations/stylelint-v14.4"),
         )
         cp.execSync("npm i", { stdio: "inherit" })
+        cp.execSync(
+            "npx rimraf ./node_modules/stylelint-config-recommended-vue",
+            { stdio: "inherit" },
+        )
+        cp.execSync(`npm i ${tgzName}`, { stdio: "inherit" })
     })
     after(() => {
         process.chdir(originalCwd)
     })
 
     it("should lint without errors", () => {
-        cp.execSync(`${STYLELINT} src/valid.vue --fix`, {
-            stdio: "inherit",
-        })
+        cp.execSync(`${STYLELINT} src/valid.vue`, { stdio: "inherit" })
     })
     it("should lint with errors", () => {
         try {
-            cp.execSync(`${STYLELINT} src/invalid.vue`, {
-                stdio: "inherit",
-            })
+            cp.execSync(`${STYLELINT} src/invalid.vue`, { stdio: "inherit" })
         } catch {
             // Expected!
             return
